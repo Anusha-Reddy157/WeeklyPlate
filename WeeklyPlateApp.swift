e73e00edb04ffe2a8a3aab4ccae49698d1e015b3
Anusha-Reddy157 <anushareddy9942@gmail.com>
2026-04-10 17:22:03 -0500

    Initial commit

---
+++ WeeklyPlateApp.swift
@@ -0,0 +1,22 @@
+//
+//  WeeklyPlateApp.swift
+//  WeeklyPlate
+//
+//  Created by Anusha Reddy kothapeta on 3/26/26.
+//
+/*
+**Student Name: Anusha Reddy Kothapeta **
+**Z-ID: Z2081200**
+**Partner Name (if applicable): Ramya Sri Kadiyala**
+**Partner Z-ID (if applicable): Z2039166**
+*/
+import SwiftUI
+
+@main
+struct WeeklyPlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
